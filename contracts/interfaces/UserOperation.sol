a07ff5624398a31c71ed9aaca9f2a63a33508272
dream.huang <xx@xx.com>
2022-12-29 17:03:47 +0800

    feat: 添加 UserOperation 对象注释

--- contracts/interfaces/UserOperation.sol
+++ contracts/interfaces/UserOperation.sol
@@ -3,33 +3,35 @@ pragma solidity ^0.8.12;
 
 /* solhint-disable no-inline-assembly */
 
-    /**
-     * User Operation struct
-     * @param sender the sender account of this request
-     * @param nonce unique value the sender uses to verify it is not a replay.
-     * @param initCode if set, the account contract will be created by this constructor
-     * @param callData the method call to execute on this account.
-     * @param verificationGasLimit gas used for validateUserOp and validatePaymasterUserOp
-     * @param preVerificationGas gas not calculated by the handleOps method, but added to the gas paid. Covers batch overhead.
-     * @param maxFeePerGas same as EIP-1559 gas parameter
-     * @param maxPriorityFeePerGas same as EIP-1559 gas parameter
-     * @param paymasterAndData if set, this field hold the paymaster address and "paymaster-specific-data". the paymaster will pay for the transaction instead of the sender
-     * @param signature sender-verified signature over the entire request, the EntryPoint address and the chain ID.
-     */
-    struct UserOperation {
+// 参考文档：
+// https://medium.com/infinitism/erc-4337-account-abstraction-without-ethereum-protocol-changes-d75c9d94dc4a
+/**
+*用户操作结构
+*@param sender 本次请求的发起人钱包地址
+*@param nonce 发送者用来验证它不是重播的唯一值。和我们之前钱包的 nonce 值一样，会按照 nonce 严格执行
+*@param initCode 如果设置，账户合约将由此构造函数创建，如果钱包尚不存在，则用于创建钱包的初始化代码
+*@param callData 要在此帐户上执行的方法调用，实际执行步骤用什么数据调用钱包
+*@param verificationGasLimit gas 用于 validateUserOp 和 validatePaymasterUserOp
+*@param preVerificationGas gas 不是通过 handleOps 方法计算的，而是添加到支付的 gas 中。涵盖批量开销。
+*@param maxFeePerGas 与 EIP-1559 gas 参数相同
+*@param maxPriorityFeePerGas 与 EIP-1559 gas 参数相同
+*@param paymasterAndData 如果设置，该字段保存付款人地址和“付款人特定数据”。出纳员将代替发件人支付交易费用
+*@param signature 对整个请求、EntryPoint 地址和链 ID 的发件人验证签名。
+*/
+struct UserOperation {
 
-        address sender;
-        uint256 nonce;
-        bytes initCode;
-        bytes callData;
-        uint256 callGasLimit;
-        uint256 verificationGasLimit;
-        uint256 preVerificationGas;
-        uint256 maxFeePerGas;
-        uint256 maxPriorityFeePerGas;
-        bytes paymasterAndData;
-        bytes signature;
-    }
+    address sender;
+    uint256 nonce;
+    bytes initCode;
+    bytes callData;
+    uint256 callGasLimit;
+    uint256 verificationGasLimit;
+    uint256 preVerificationGas;
+    uint256 maxFeePerGas;
+    uint256 maxPriorityFeePerGas;
+    bytes paymasterAndData;
+    bytes signature;
+}
 
 library UserOperationLib {
 
